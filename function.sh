cb3c0dafd5962691604c695d8c6373547700c21c
amankumar1988 <kumar.aman6600@gmail.com>
2023-03-09 07:40:41 +0530

    new

--- function.sh
+++ function.sh
@@ -12,7 +12,8 @@ stat()
 {
     echo "Total number of sessions: $(who | wc -l)"
     echo "Today date is $(date +%F)"
+    echo "Load Average on the system is $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1})
 }
 
-sample
+echo "calling stat function"
 stat
