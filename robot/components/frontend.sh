7919ff7ad6f72afd872ee4a89c5164bf8046cc5b
amankumar1988 <kumar.aman6600@gmail.com>
2023-03-10 08:01:43 +0530

    new

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -6,22 +6,22 @@ set -e
 USERID=$(id -u)
 
 if [ "$USERID" -ne 0 ];then
-    echo -e "\e[31m You should execute this script as root user or with sudo as prefix \e[0m]"
+    echo -e "\e[31m You should execute this script as root user or with sudo as prefix \e[0m"
     exit 1
 fi
 
 
-yum install nginx -y
+yum install nginx -y &>> "/tmp/frontend.log"
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
+rm -rf * &>> "/tmp/frontend.log"
+unzip /tmp/frontend.zip  &>> "/tmp/frontend.log"
 mv frontend-main/* .
 mv static/* .
-rm -rf frontend-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+rm -rf frontend-main README.md &>> "/tmp/frontend.log"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> "/tmp/frontend.log"
 
 systemctl enable nginx
 systemctl start nginx
