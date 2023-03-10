69b14e2f527ac35b4374b984f8d40d09372c9004
amankumar1988 <kumar.aman6600@gmail.com>
2023-03-10 08:24:53 +0530

    new

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -33,7 +33,7 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans
 stat $?
 
 echo -n "Installing mongodb-org: "
-yum install mongodb-org -y &>> $LOGFILE
+yum install -y mongodb-org -y &>> $LOGFILE
 stat $?
 
 echo -n "Start mongodb: "
