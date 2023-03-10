03b073a7273df04c40adb8e75c6d99da62f3ad46
amankumar1988 <kumar.aman6600@gmail.com>
2023-03-10 08:47:24 +0530

    new

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -28,16 +28,19 @@ fi
 }
 
 echo -n "Downloading the $COMPONENT component :"
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> $LOGFILE
 stat $?
 
 echo -n "Installing nodejs : "
-yum install -y  nodejs y &>> $LOGFILE
+yum install nodejs -y &>> $LOGFILE
 stat $?
 
-echo -n "Create user :"
-useradd $APPUSER &>> $LOGFILE
-stat $?
+id $APPUSER
+if [ $? -ne 0 ];then
+    echo -n "Create user :"
+    useradd $APPUSER &>> $LOGFILE
+    stat $?
+fi
 
 echo -n "Downloading the $COMPONENT zip :"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
