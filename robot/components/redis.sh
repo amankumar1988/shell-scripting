fe08142867c2460a9325778418d3d7d76484afee
amankumar1988 <kumar.aman6600@gmail.com>
2023-03-13 07:50:47 +0530

    new

---
+++ robot/components/redis.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "this is mongodb"
+
+#!/bin/bash
+
+set -e
+COMPONENT="redis"
+LOGFILE="/tmp/redis"
+
+stat()
+{
+if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSuccess \e[0m"
+else
+    echo -e "\e[32mFailure \e[0m"
+    exit 2
+fi
+}
+
+echo -n "Configuring $COMPONENT repo: "
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+stat $?
+
+echo -n "Installing $COMPONENT : "
+yum install redis-6.2.11 -y
+stat $? &>> $LOGFILE
+
+echo -n "Updating the $COMPONENT visibility: "
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+stat $? 
+
+echo -n "Performing daemon-reload :"
+systemctl daemon-reload &>> $LOGFILE
+systemctl restart mongod &>> $LOGFILE
+stat $?
