35112ff3f4ad8e28564222aa7f3f2aa84b2ca8e3
amankumar1988 <kumar.aman6600@gmail.com>
2023-03-14 08:10:55 +0530

    new

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -21,9 +21,21 @@ echo -n "Grab $COMPONENT default pswd: "
 DEFAULT_ROOT_PWD=$(grep "temporary pass" /var/log/mysqld.log | awk '{print $NF}')
 stat $?
 
-echo -n "Password reset of root user :"
+
+echo "show databases;" | mysql -uroot -pRoboshop@1  &>> $LOGFILE
+if [ $? -ne 0 ];then
+
+echo -n "Password Reset of root user :"
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PWD} &>> $LOGFILE 
 stat $?
 
+fi
 
 
+echo "show plugins;" | mysql -uroot -pRoboshop@1 | grep validate_password  &>> $LOGFILE
+if [ $? -ne 0 ];then
+
+echo -n "Uninstalling Paswd validation plugin :"
+echo "uninstall plugin validate_password;" | mysql -uroot -pRoboshop@1  &>> $LOGFILE
+
+fi
