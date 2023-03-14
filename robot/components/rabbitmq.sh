f1b758b30b457c1e56324d582f84887c35d23bb9
amankumar1988 <kumar.aman6600@gmail.com>
2023-03-14 20:25:57 +0530

    new

--- robot/components/rabbitmq.sh
+++ robot/components/rabbitmq.sh
@@ -35,3 +35,10 @@ id $APPUSER
         stat $?
     fi
 }
+
+echo -n "add rabbitmq user :"
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_user_tags roboshop administrator
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+stat $?
