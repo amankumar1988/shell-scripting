87bc1c36def7cdeb16aa23886fd3d996798cf4b4
amankumar1988 <kumar.aman6600@gmail.com>
2023-03-14 08:53:27 +0530

    new

--- robot/components/common.sh
+++ robot/components/common.sh
@@ -55,7 +55,97 @@ NPM_INSTALL(){
 
 CONFIG_SVC(){
     echo -n "Updating the systemd file with DB Details :"
-    sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+    sed -i -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/'  -e  's/CARTHOST/cart.roboshop.internal/' -e  's/DBHOST/mysql.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+    mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+    stat $? 
+
+    echo -n "Starting the $COMPONENT service : "
+    systemctl daemon-reload &>> $LOGFILE
+    systemctl enable $COMPONENT &>> $LOGFILE
+    systemctl restart $COMPONENT &>> $LOGFILE
+    stat $?
+
+}
+
+MVN_PACKAGE() {
+    echo -n "Creating the $COMPONENT Package :"
+    cd /home/$APPUSER/$COMPONENT/ 
+    mvn clean package  &>> $LOGFILE
+    mv target/shipping-1.0.jar shipping.jar
+    stat $?   
+}
+
+PYTHON() {
+    echo -n "Installing Python and dependencies :"
+    yum install python36 gcc python3-devel -y  &>> $LOGFILE
+    stat $?
+
+    # Calling Create-User Functon 
+    CREATE_USER
+
+    # Calling Download_And_Extract Function
+    DOWNLOAD_AND_EXTRACT
+
+    echo -n "Installing $COMPONENT :"
+    cd /home/roboshop/$COMPONENT/ 
+    pip3 install -r requirements.txt   &>> $LOGFILE 
+    stat $? 
+
+    USERID=$(id -u roboshop)
+    GROUPID=$(id -g roboshop)
+    
+    echo -n "Updating the $COMPONENT.ini file :"
+    sed -e "/^uid/ c uid=${USERID}" -e "/^gid/ c gid=${GROUPID}"  /home/$APPUSER/$COMPONENT/$COMPONENT.ini 
+
+    # Calling Config-Svc Function
+    CONFIG_SVC
+
+}
+
+JAVA() {
+    echo -n "Installing Maven  :" 
+    yum install maven -y &>> $LOGFILE
+    stat $?
+
+    # Calling Create-User Functon 
+    CREATE_USER
+
+    # Calling Download_And_Extract Function
+    DOWNLOAD_AND_EXTRACT
+
+    # Calling Maven Package Functon
+    MVN_PACKAGE
+
+    # Calling Config-Svc Function
+    CONFIG_SVC
+
+}
+
+
+NODEJS() {
+
+    echo -n "Configuring the nodejs repo :"
+    curl --silent --location https://rpm.nodesource.com/setup_16.x | bash - &>> $LOGFILE
+    stat $?  
+
+    echo -n "Installing NodeJS :"
+    yum install nodejs -y &>> $LOGFILE
+    stat $?
+
+    # Calling Create-User Functon 
+    CREATE_USER
+
+    # Calling Download_And_Extract Function
+    DOWNLOAD_AND_EXTRACT
+
+    # Calling NPM Install Function
+    NPM_INSTALL
+
+    # Calling Config-Svc Function
+    CONFIG_SVC
+
+}
+
     mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
     stat $? 
 
@@ -94,6 +184,15 @@ PYTHON(){
     pip3 install -r requirements.txt  &>> $LOGFILE
     stat $?
 
+    USERID=$(id -u roboshop)
+    GROUPID=$(id -g roboshop)
+    echo -n "Updating the $COMPONENT.ini file :"
+    sed -e "/^uid/ c uid=${USERID}" -e "/^gid/ c gid=${GROUPID}"  /home/$APPUSER/$COMPONENT/$COMPONENT.ini 
+    stat $?
+
+    echo -n "Configure service :"
+    CONFIG_SVC
+    stat $?
 
 }
 
