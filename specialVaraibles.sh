17bfd76fcf2139d3d8f65fb51b54a5d6cf8caec3
amankumar1988 <kumar.aman6600@gmail.com>
2023-03-15 07:32:33 +0530

    new

---
+++ specialVaraibles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+# Special Characters :  $0 to $9 , $* , $# , #@ , $$   
+a=10
+
+echo "Value of a is $a"
+
+echo "Name of the script is : $0"     # Gives the name of the script you're running  
+
+echo first value is $1
+echo second value is $2 
+echo third value is $3
+
+# bash scriptName.sh 100 200 300
