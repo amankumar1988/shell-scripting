14cacd5747219e6827a7efe24fc5065ed0208ee4
amankumar1988 <kumar.aman6600@gmail.com>
2023-03-16 21:48:29 +0530

    new

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -11,7 +11,12 @@ fi
 COMPONENT=$1 
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b53-allow-all-sg | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 echo "Ami ID is $AMI_ID "
 
 echo -n "Launching the instance with $AMI_ID as AMI :"
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq 
+aws ec2 run-instances \
+    --image-id ${AMI_ID} \
+    --instance-type t2.micro \
+    --security-group-ids ${SGID} \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq 
