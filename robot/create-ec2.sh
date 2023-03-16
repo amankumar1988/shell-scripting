7e1c469fdb6c7bbb4d1b8c444e56f378b79b5ddf
amankumar1988 <kumar.aman6600@gmail.com>
2023-03-16 22:13:45 +0530

    new

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -8,6 +8,7 @@ if [ -z "$1" ]; then
     exit 1
 fi 
 
+HOSTEDZONEID="Z03162902ZBUWVJLMZQNI"
 COMPONENT=$1 
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
@@ -15,9 +16,13 @@ SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b53-all
 echo "Ami ID is $AMI_ID "
 
 echo -n "Launching the instance with $AMI_ID as AMI :"
-aws ec2 run-instances \
+IPADDRESS=$(aws ec2 run-instances \
     --image-id ${AMI_ID} \
     --instance-type t2.micro \
     --security-group-ids ${SGID} \
     --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent, InstanceInterruptionBehavior=stop}" \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq 
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's)
+
+
+sed -e "s/COMPONENT/${COMPONENT}" -e "/IPADDRESS/${IPADDRESS}" route53.jsob > /tmp/r53.json
+aws route53 change-resource-record-sets --host-zone-id $HOSTEDZONEID --change-batch file://r53.json
