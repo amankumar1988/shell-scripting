ff72139e9d3ce28dabcdb12e983f6a6f54782c68
amankumar1988 <kumar.aman6600@gmail.com>
2023-03-26 11:27:54 +0530

    Autopush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -13,16 +13,32 @@ COMPONENT=$1
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b53-allow-all-sg | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
-echo "Ami ID is $AMI_ID "
+echo "Ami ID is $AMI_ID"
 
 echo -n "Launching the instance with $AMI_ID as AMI :"
-IPADDRESS=$(aws ec2 run-instances \
-    --image-id ${AMI_ID} \
-    --instance-type t2.micro \
-    --security-group-ids ${SGID} \
-    --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent, InstanceInterruptionBehavior=stop}" \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-echo $IPADDRESS
-sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" robot/record.json > /tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/record.json | jq
+create_server() {
+    
+    echo "*** Launching $COMPONENT Server ***"
+
+    IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID \
+                    --instance-type t3.micro \
+                    --security-group-ids ${SGID} \
+                    --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+                    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+    sed -e "s/COMPONENT/${COMPONENT}/" -e  "s/IPADDRESS/${IPADDRESS}/" robot/record.json > /tmp/record.json
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/record.json | jq
+
+    echo "*** $COMPONENT Server Completed ***"
+
+}
+
+if [ "$1" == "all" ] ; then 
+    for component in frontend mongodb catalogue cart user mysql redis rabbitmq shipping payment ; do  
+        COMPONENT=$component 
+        create_server
+    done
+else 
+        exit 1
+fi 
